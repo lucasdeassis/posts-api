ea2d2fa45d2ffaf65f8dd192c15fefde27014ec0
lucasdeassis <lucasassis413@gmail.com>
2018-07-17 14:27:44 -0300

    push actions and reducers

---
+++ example/src/actions/index.js
@@ -0,0 +1,41 @@
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_POST = 'SELECT_POST'
+
+const API_URL = 'http://localhost';
+const API_PORT = '3000';
+
+export const selectPost = post => ({
+  type: SELECT_POST,
+  post
+})
+
+export const requestPosts = user => ({
+  type: REQUEST_POSTS,
+  user
+})
+
+export const receivePosts = (user, json) => ({
+  type: RECEIVE_POSTS,
+  user,
+  posts: json.filter(post => post.userEmail === user.email),
+})
+
+const fetchPosts = user => dispatch => {
+  dispatch(requestPosts(user))
+  return fetch(`${API_URL}:${API_PORT}/posts`)
+    .then(response => response.json())
+    .then(json => dispatch(receivePosts(user, json)))
+}
+
+const shouldFetchPosts = (state, user) => {
+  const posts = state.postsByUser[user]
+
+  return !posts || !posts.isFetching;
+}
+
+export const fetchPostsIfNeeded = user => (dispatch, getState) => {
+  if (shouldFetchPosts(getState(), user)) {
+    return dispatch(fetchPosts(user))
+  }
+}
---
+++ example/src/reducers/index.js
@@ -0,0 +1,55 @@
+import { combineReducers } from 'redux'
+import {
+  SELECT_POST,
+  REQUEST_POSTS, RECEIVE_POSTS
+} from '../actions'
+
+const selectedPost = (state = '', action) => {
+  switch (action.type) {
+    case SELECT_POST:
+      return action.post
+    default:
+      return state
+  }
+}
+
+const posts = (state = {
+  isFetching: false,
+  items: []
+}, action) => {
+  switch (action.type) {
+    case REQUEST_POSTS:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        isFetching: false,
+        items: action.posts,
+      }
+    default:
+      return state
+  }
+}
+
+const postsByUser = (state = { }, action) => {
+  switch (action.type) {
+    case RECEIVE_POSTS:
+    case REQUEST_POSTS:
+      return {
+        ...state,
+        [action.email]: posts(state[action.email], action)
+      }
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  postsByUser,
+  selectedPost
+})
+
+export default rootReducer
