c7f04f4e9dfa1596546dcb72211347356c4dc3de
lucasdeassis <lucasassis413@gmail.com>
2018-07-17 15:53:14 -0300

    change index to .js and api port

--- example/src/actions/index.js
+++ example/src/actions/index.js
@@ -3,7 +3,7 @@ export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 export const SELECT_POST = 'SELECT_POST';
 
 const API_URL = 'http://localhost';
-const API_PORT = '3000';
+const API_PORT = '8080';
 
 export const selectPost = post => ({
   type: SELECT_POST,
