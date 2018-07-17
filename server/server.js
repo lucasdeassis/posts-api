de55625e7820d4abafb8630b1b65b8ee24922def
lucasdeassis <lucasassis413@gmail.com>
2018-07-17 11:59:17 -0300

    create post model and endpoints

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 
-var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+var { User } = require('./models/user');
+var { Post } = require('./models/post');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -20,14 +22,10 @@ app.post('/register', async (req, res) => {
     const user = new User(body);
     await user.save();
     const token = await user.generateAuthToken();
-    res.header('x-auth', token).send(user);
-  } catch (e) {
-    res.status(400).send(e);
+    res.header('x-auth', token).status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
   }
-});
-
-app.get('/me', authenticate, (req, res) => {
-  res.send(req.user);
 });
 
 app.post('/login', async (req, res) => {
@@ -36,22 +34,55 @@ app.post('/login', async (req, res) => {
     const user = await User.findByCredentials(body.email, body.password);
     const token = await user.generateAuthToken();
     res.header('x-auth', token).send(user);
-  } catch (e) {
+  } catch (err) {
     res.status(400).send();
   }
 });
 
-app.delete('/me/token', authenticate, async (req, res) => {
+app.post('/create/post', authenticate, async (req, res) => {
   try {
-    await req.user.removeToken(req.token);
-    res.status(200).send();
+    const body = _.pick(req.body, ['title', 'text']);
+    const post = new Post({ ...body, userFirstName: req.user.firstName  });
+    await post.save();
+    res.status(201).send(post);
+  } catch(err) {
+    res.status(400).send(err);
+  }
+});
+
+app.get('/posts', authenticate, async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.send({ posts });
+  } catch(err) {
+    res.status(400).send(err);
+  }
+});
+
+app.delete('/post/:id', authenticate, async (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  try {
+    const post = await Post.findOneAndRemove({
+      _id: id,
+    });
+
+    if (!post) {
+      return res.status(404).send();
+    }
+
+    res.send({ post });
   } catch (e) {
     res.status(400).send();
   }
 });
 
 app.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+  console.log(`Started up at port ${port}`); // eslint-disable-line no-console
 });
 
-module.exports = {app};
+module.exports = { app };
