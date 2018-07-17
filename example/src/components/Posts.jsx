6a28cc5ea838ebdd9ba4fe08f7e5168b75a81ae0
lucasdeassis <lucasassis413@gmail.com>
2018-07-17 19:12:18 -0300

    add more info into post component

--- example/src/components/Posts.jsx
+++ example/src/components/Posts.jsx
@@ -3,16 +3,32 @@ import PropTypes from 'prop-types';
 
 const Posts = ({ posts }) => (
   <ul>
-    {posts.map((post, i) => (
-      <li key={i}>
-        {post.title}
+    {posts.map(post => (
+      <li key={post._id}>
+        <li>
+          {post.title}
+        </li>
+        <ul>
+          <li>
+            {`(${post.text})`}
+          </li>
+          <li>
+            {`user: ${post.userName}`}
+          </li>
+          <li>
+            {`email: ${post.userEmail}`}
+          </li>
+          <li>
+            {`createdAt: ${new Date(post.createdAt).toLocaleString()}`}
+          </li>
+        </ul>
       </li>
     ))}
   </ul>
 );
 
 Posts.propTypes = {
-  posts: PropTypes.array.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Posts;
