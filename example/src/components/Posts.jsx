d22ee631c876a42a2044d6b9eb76a8924f64f2db
lucasdeassis <lucasassis413@gmail.com>
2018-07-19 04:19:03 -0300

    add delete button and use destructuring

--- example/src/components/Posts.jsx
+++ example/src/components/Posts.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Posts = ({ posts }) => (
+const Posts = ({ posts, deletePost }) => (
   <ul>
-    {posts.map(post => (
-      <li key={post._id}>
-        {`Title: ${post.title}`}
-        <ul>
-          <li>
-            {`Text: ${post.text}`}
-          </li>
-          <li>
-            {`User: ${post.userName}`}
-          </li>
-          <li>
-            {`E-mail: ${post.userEmail}`}
-          </li>
-          <li>
-            {`Created At: ${new Date(post.createdAt).toLocaleString()}`}
-          </li>
-        </ul>
-      </li>
-    ))}
+    {
+      posts.map(({
+        _id,
+        title,
+        text,
+        userName,
+        userEmail,
+        createdAt
+      }) => (
+        <li key={_id}>
+          {`Title: ${title}`}
+          <ul>
+            <li>
+              {`Text: ${text}`}
+            </li>
+            <li>
+              {`User: ${userName}`}
+            </li>
+            <li>
+              {`E-mail: ${userEmail}`}
+            </li>
+            <li>
+              {`Created At: ${new Date(createdAt).toLocaleString()}`}
+            </li>
+            <li>
+              <button type="button" onClick={() => deletePost(_id)}>
+              Delete Post
+              </button>
+            </li>
+          </ul>
+          <br />
+        </li>
+      ))
+    }
   </ul>
 );
 
 Posts.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 export default Posts;
